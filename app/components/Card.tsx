00ed6d93236a71411d30cbaedf8b6a5f9b4d57cb
Ysel84 <yannselosse@gmail.com>
2025-04-13 22:08:41 +0200

    code cleaning

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -11,7 +11,7 @@ export default function Card({
     return (
         <View style={styles.card}>
             <Image
-                source={ imageSource}
+                source={imageSource}
                 style={styles.cardImage}
                 contentFit="cover"
             />
