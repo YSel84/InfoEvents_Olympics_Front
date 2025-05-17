16a5f24fd77741574b0d6e65333316040ec2ec4f
Ysel84 <yannselosse@gmail.com>
2025-05-17 08:43:27 +0200

    tweaks

--- app.config.js
+++ app.config.js
@@ -5,10 +5,16 @@ export default {
         name: 'InfoEvents_OlympicsTicketing',
         slug: 'InfoEvents_OlympicsTicketing',
         version: '1.0.0',
+        android: {
+            package: 'com.ysel84.infoevents_ticketing',
+        },
         extra: {
+            eas: {
+                projectId: '31e68330-576a-47c9-a7db-78c7f91398d2',
+            },
             API_BASE_URL:
                 process.env.EXPO_PUBLIC_API_BASE_URL ??
-                'http://localhost:8080/api',
+                'http://localhost:5000/api',
             FRONT_USERNAME: process.env.EXPO_PUBLIC_FRONT_USERNAME,
             FRONT_PASSWORD: process.env.EXPO_PUBLIC_FRONT_PASSWORD,
         },
